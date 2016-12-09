00795e7cfef5e07d6c70d0fdcc3dca3953b29c9d
Jeffrey Blatt <jeff@piuteponds.org>
2016-12-08 21:48:02 -0800

    Recent updates

---
+++ src/team.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<?php
+include_once('php/piute_includes.php');
+session_start();
+?>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Piute Ponds Home Page</title>
+
+<link href="PiuteMasterCSS.css" rel="stylesheet" type="text/css" />
+<style type="text/css">
+<!--
+.style10 {color: #003300}
+.style12 {color: #000000; font-size: 12px; }
+.style13 {font-size: 18pt}
+.style15 {font-size: x-large}
+-->
+</style>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<div id="BasePage"><img src="images/Piute_Background_Image.jpg" width="720" height="778" alt="PiutePonds-Background_Image" /></div>
+<div class="rollover" id="apDiv3">
+  <div class="style3">
+    <p><!-- #BeginLibraryItem "/Library/left-nav-buttons.lbi" -->
+<p><!-- #BeginLibraryItem "/Library/left-nav-buttons.lbi" --><div class="rollover" id="buttons">
+  <div class="rollover"><a href="index.php" class="rollover">HOME</a></div>
+  <div class="rollover"><a href="history.php"> HISTORY OF<br>
+    PIUTE PONDS</a></div>
+  <div class="rollover"><a href="location.php">LOCATION</a></div>
+  <div class="rollover"><a href="birding.php">BIRDING</a></div>
+  <div class="rollover"><a href="photography.php"> PHOTOGRAPHY</a></div>
+  <div class="rollover"><a href="hunting.php">HUNTING</a></div>
+  <div class="rollover"><a href="volunteer.php">VOLUNTEERS</a></div>
+  <div class="rollover"><a href="educators.php">EDUCATORS</a></div>
+  <div class="rollover"><a href="natural_resources.php">NATURAL RESOURCES</a></div>
+  <div class="rollover"><a href="friends-of-piute-ponds.php">FRIENDS OF PIUTE PONDS</a></div>
+  <div class="rollover"><a href="audobon-society.php">AUDUBON SOCIETY</a></div>
+  <div class="rollover"><a href="district-14.php">DISTRICT 14</a></div>
+  <div class="rollover"><a href="ducks-unlimited.php">DUCKS UNLIMITED</a></div>
+  <div class="rollover"><a href="edwards-afb.php">        EDWARDS AFB</a></div>
+  <div class="rollover"><a href="partners-in-flight.php">PARTNERS IN FLIGHT</a></div>
+  <div class="rollover"><a href="contact-us.php">CONTACT US</a></div>
+  <div class="rollover"></div>
+  </div>
+<!-- #EndLibraryItem --></p>
+<p><a href="https://www.facebook.com/pages/Friends-of-Piute-Ponds/171112009611901" target="_blank"><img src="images/FacebookIcon.jpg" alt="Facebook Page" width="45" height="44"></a></p>
+<!-- #EndLibraryItem --><p class="style9">&nbsp;</p>
+    <p class="style9">&nbsp;</p>
+</div>
+</div>
+<div id="apDiv1">
+  <p align="left" class="style4">Friends of Piute Ponds</p>
+  <p align="left" class="style9">CEO: Wanda Deal</p>
+  <p align="left" class="style9">CFO: Jeffrey Blatt</p>
+  <p align="left" class="style9">Secretary: Dianne Satturlee</p>
+  <p align="left" class="style9">Webmaster: Dianne Satturlee</p>
+  <p align="left" class="style9">PHP Development: Jeffrey Blatt</p>
+  <p align="left" class="style9">Python Development: Jeffrey Blatt </p>
+  <p align="left" class="style9">DBA: Jeffrey Blatt&nbsp;</p>
+  <p align="left" class="style9">Feel free to click the button and donate to our non-profit organization.</p>
+  <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="P6E43GCBT86MC">
+<input name="submit" type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" alt="PayPal - The safer, easier way to pay online!" align="middle" border="0" />
+<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+  </form>
+<p align="left" class="style9">&nbsp;</p>
+  
+  
+  <p align="left" class="style4">&nbsp;</p>
+<p class="style4">&nbsp;</p>
+<p class="style4">&nbsp;</p>
+</div>
